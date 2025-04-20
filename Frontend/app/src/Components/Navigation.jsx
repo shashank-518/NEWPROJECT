9c2c515ce57c5ffdf3439a1bc7f11990bce9ffdb
Shashank S <shashank5418shashu@gmail.com>
2025-04-20 19:46:20 +0530

    First Commit

---
+++ Frontend/app/src/Components/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './Navigation.css'
+
+export default Navigation = () => {
+    return (
+        <>
+            <nav className="navbar">
+
+                <div>Hello</div>
+                <div><ul>
+                    <li>Home</li>
+                    <li>Order Item</li>
+                    <li>Contact</li>
+                </ul></div>
+                <div>Byye</div>
+
+                
+            </nav>
+        
+        </>
+    )
+}
