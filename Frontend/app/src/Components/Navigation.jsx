3fdea808b7d19b8b2d414100e034892fcf63bf26
Shashank S <shashank5418shashu@gmail.com>
2025-04-20 20:11:49 +0530

    Building navbar

--- Frontend/app/src/Components/Navigation.jsx
+++ Frontend/app/src/Components/Navigation.jsx
@@ -6,12 +6,17 @@ export default Navigation = () => {
         <>
             <nav className="navbar">
 
-                <div>Hello</div>
-                <div><ul>
+                <div className="firstpart" >
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/best-cow-names-66f573deac313.png?crop=0.473xw:0.713xh;0.0208xw,0.140xh&resize=1200:*" alt="image" />
+                    <h1>Nandini</h1>
+                </div>
+                <div className="mainpart">
+                    <ul>
                     <li>Home</li>
                     <li>Order Item</li>
                     <li>Contact</li>
-                </ul></div>
+                </ul>
+                </div>
                 <div>Byye</div>
 
                 
